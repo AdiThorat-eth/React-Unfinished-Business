75e9cd3aa6752eec7796d371afecba1acfc97fde
Aditya Thorat <a.workspace.00@gmail.com>
2025-09-28 15:53:23 +0530

    used arrow fxn for useState()

--- 03 state management and useState hook()/src/App.jsx
+++ 03 state management and useState hook()/src/App.jsx
@@ -2,16 +2,24 @@ import React, { useState } from "react";
 
 const App = () => {
   const [score, setScore] = useState(0);
-  setScore(150);
+  // setScore(150);
 
   return (
     <div className="h-screen w-full">
       <h1 className="text-5xl text-center mt-10">{score}</h1>
       <div className="flex justify-center item-center">
-        <button 
-        onClick={setScore}
-        // we have to write setScore() but duw to bracker "()" it will execute immediately but we want it to execute onClick but if we just write setScore then it is not fxn so declare setScore before return() and then write setScore in onClick 
-        className="px-4 py-2 bg-amber-200 mt-10 rounded-full text-3xl">change</button>
+        <button
+          // onClick={setScore}
+          // we have to write setScore() but duw to bracker "()" it will execute immediately but we want it to execute onClick but if we just write setScore then it is not fxn so declare setScore before return() and then write setScore in onClick
+
+          onClick={() => {
+            setScore(200);
+          }}
+          // In this we have given a fxn to onClick which will be executed by onClick and in that fxn will set setScore
+          className="px-4 py-2 bg-amber-200 mt-10 rounded-full text-3xl"
+        >
+          change
+        </button>
       </div>
     </div>
   );
