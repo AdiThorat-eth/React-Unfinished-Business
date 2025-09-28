fe46ecf225d09333fc14c5b8b4185c16aa0edcda
Aditya Thorat <a.workspace.00@gmail.com>
2025-09-28 16:01:17 +0530

    implemented useState to change from T2F and F2T

--- 03 state management and useState hook()/src/App.jsx
+++ 03 state management and useState hook()/src/App.jsx
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [score, setScore] = useState(0);
-  // setScore(150);
+  const [banned, setBanned] = useState(false);
 
   return (
     <div className="h-screen w-full">
-      <h1 className="text-5xl text-center mt-10">{score}</h1>
+      {/*we cant directly write boolean in h1 so we have to convert it to String */}
+      <h1 className="text-5xl text-center mt-10">{banned.toString()}</h1>
       <div className="flex justify-center item-center">
         <button
-          // onClick={setScore}
-          // we have to write setScore() but duw to bracker "()" it will execute immediately but we want it to execute onClick but if we just write setScore then it is not fxn so declare setScore before return() and then write setScore in onClick
-
           onClick={() => {
-            setScore(200);
+            setBanned(!banned);
           }}
           // In this we have given a fxn to onClick which will be executed by onClick and in that fxn will set setScore
           className="px-4 py-2 bg-amber-200 mt-10 rounded-full text-3xl"
