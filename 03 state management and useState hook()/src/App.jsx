001fa7054ffe282e47a295e842de993a4a2f5919
Aditya Thorat <a.workspace.00@gmail.com>
2025-09-28 17:40:02 +0530

    implemented interchanging setVal and marks increment in array data

--- 03 state management and useState hook()/src/App.jsx
+++ 03 state management and useState hook()/src/App.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [val, setVal] = useState({ name: "Abc", isRunner: true });
+  const [val, setVal] = useState({ name: "Abc", isRunner: true, marks: 11 });
 
   return (
     <div className="text-center text-3xl m-4">
       <h1>name: {val.name}</h1>
       {/* isRunner is boolean to use it in h1 we have to convert it into string */}
       <h2>isRunner: {val.isRunner.toString()}</h2>
+      <h3>Marks: {val.marks}</h3>
       <button
         onClick={() => {
-          setVal({ ...val, isRunner: !val.isRunner });
+          setVal({ ...val, marks: val.marks + 1, isRunner: !val.isRunner });
         }}
         className="px-3 py-1 bg-sky-200 rounded-full"
       >
