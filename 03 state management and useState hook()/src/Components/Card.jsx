9e5fca7ee909a798fd1732706269e75f05e5f67c
Aditya Thorat <a.workspace.00@gmail.com>
2025-09-29 08:17:32 +0530

    created a img interchangable compo using useState

--- 03 state management and useState hook()/src/Components/Card.jsx
+++ 03 state management and useState hook()/src/Components/Card.jsx
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
+import { FaCircleArrowRight } from "react-icons/fa6";
 
 const Card = () => {
   const [val, setVal] = useState(false);
+
   return (
-    <div className="text-center">
-      {/* print "go outside" if val is false and print "dont go" if val is true */}
-      <h1 className="text-center m-5">
-        {val === false ? "go outside" : "dont go"}
-      </h1>
-
-      <button
-        // onClick={() => {setVal(!val)}}
-        // it is not good practice
-
-        onClick={() => setVal(() => !val)}
-        // this is coorect way
-        className="px-5 py-2 bg-yellow-300 rounded-full"
-      >
-        Change
-      </button>
+    <div className="h-screen w-full bg-zinc-300 flex justify-center items-center">
+      <div className="relative h-32 w-60 rounded-lg bg-lime-200 overflow-hidden flex">
+        <img
+          className={`w-full h-full object-cover shrink-0 transition-transform duration-300 ease-in-out ${
+            val === false ? "-translate-x-[0%]" : "-translate-x-[100%]"
+          }`}
+          src="https://images.unsplash.com/photo-1758621518225-9248e65dbaee?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3N3x8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          className={`w-full h-full object-cover shrink-0 transition-transform duration-300  ease-in-out ${
+            val === false ? "-translate-x-[0%]" : "-translate-x-[100%]"
+          }`}
+          src="https://images.unsplash.com/photo-1758645839267-6fa279bde18f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <span
+          onClick={() => setVal(() => !val)}
+          className="text-2xl absolute -bottom-2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-red-500/80"
+        >
+          <FaCircleArrowRight />
+        </span>
+      </div>
     </div>
   );
 };
