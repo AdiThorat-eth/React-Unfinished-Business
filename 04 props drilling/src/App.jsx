45672ca67f653920df7c789543697b047bea9fb3
Aditya Thorat <a.workspace.00@gmail.com>
2025-09-29 19:48:18 +0530

    Created a functional card using props

--- 04 props drilling/src/App.jsx
+++ 04 props drilling/src/App.jsx
@@ -2,10 +2,39 @@ import React from "react";
 import Card from "./Components/Card";
 
 const App = () => {
+  const data = [
+    {
+      name: "Brook",
+      description: "Yohohoho",
+      image:
+        "https://images.unsplash.com/photo-1758535557154-af85568c1e89?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Teach",
+      description: "Zihahaha",
+      image:
+        "https://images.unsplash.com/photo-1757868483984-ebe5b16526b9?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Kaido",
+      description: "Arararara",
+      image:
+        "https://images.unsplash.com/photo-1755104572227-904d7a0758fb?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Soul",
+      description: "Derishishi",
+      image:
+        "https://images.unsplash.com/photo-1758640920730-a4b27cd8aa21?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3MHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
   return (
     <div>
-      <Card yohoho="hi" color="bg-lime-200"/>
-      <Card yohoho="hello" color="bg-amber-200"/>
+      <div className="w-full h-screen gap-4 flex items-center justify-center">
+        {data.map((item, idx) => (
+          <Card values={item} />
+        ))}
+      </div>
     </div>
   );
 };
--- 04 props drilling/src/Components/Card.jsx
+++ 04 props drilling/src/Components/Card.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
-const Card = ({ yohoho, color }) => {
+const Card = ({ values }) => {
+  const { image, name, description } = values;
   return (
-    <div className="text-center m-5">
-      <button className={`px-5 py-2 ${color} rounded-full text-sm`}>
-        {yohoho}
-      </button>
+    <div>
+      <div className="w-42 bg-zinc-300 rounded p-2 text-center">
+        <div className="h-30 w-38 bg-lime-200 rounded-lg overflow-hidden">
+          <img className="h-full w-full object-cover" src={image} alt="" />
+        </div>
+        <h1 className="text-3xl">{name}</h1>
+        <p className="text-xl">{description}</p>
+        <button
+          onClick={() => alert(`You added ${name} as a friend`)}
+          className="px-2 py-1 bg-violet-400 rounded-full font-semibold"
+        >
+          Add Friend
+        </button>
+      </div>
     </div>
   );
 };
 
 export default Card;
+// 20.11 6.42
