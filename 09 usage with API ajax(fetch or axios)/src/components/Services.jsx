cdd9b4bc889ee4235dbe17cb577ea122e7f68d4d
Aditya Thorat <a.workspace.00@gmail.com>
2025-11-06 12:34:30 +0530

    component re-rendering

--- 09 usage with API ajax(fetch or axios)/src/components/Services.jsx
+++ 09 usage with API ajax(fetch or axios)/src/components/Services.jsx
@@ -1,15 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Services = () => {
+  const [first, setfirst] = useState("this is normal data");
+  const [second, setsecond] = useState("this is very large data");
+
   useEffect(() => {
     console.log("Services component mounted");
 
     return () => {
       console.log("Services component unmounted");
     };
-  });
+  }, []);
 
-  return <div>Services</div>;
+  return (
+    <div className="text-center m-10">
+      <h1>{first}</h1>
+      <button
+        onClick={() => setfirst("Data has been changed")}
+        className="p-2 bg-violet-200 rounded mb-4"
+      >
+        Change normal data
+      </button>
+
+      <h1>{second}</h1>
+      <button
+        onClick={() => setsecond("Large data has been changed")}
+        className="p-2 bg-lime-200 rounded"
+      >
+        Change large data
+      </button>
+    </div>
+  );
 };
 
 export default Services;
