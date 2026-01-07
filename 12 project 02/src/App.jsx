88de7565afb0832fe85bd1c73997ef2dc98a5598
Aditya Thorat <a.workspace.00@gmail.com>
2026-01-07 15:01:01 +0530

    lenis applied

--- 12 project 02/src/App.jsx
+++ 12 project 02/src/App.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Stripes from "./Components/Stripes";
@@ -8,6 +12,36 @@ import Cards from "./Components/Cards";
 import Footer from "./Components/Footer";
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: "vertical",
+      gestureOrientation: "vertical",
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+      syncTouch: true, // Add this for better touch sync
+      touchInertiaMultiplier: 35, // Add this for better touch physics
+    });
+
+    // Connect Lenis with ScrollTrigger
+    lenis.on("scroll", ScrollTrigger.update);
+
+    const ticker = (time) => {
+      lenis.raf(time * 1000);
+    };
+
+    gsap.ticker.add(ticker);
+
+    return () => {
+      lenis.destroy();
+      gsap.ticker.remove(ticker);
+    };
+  }, []);
+
   return (
     <div className="bg-zinc-900 font-Satoshi">
       <Navbar />
