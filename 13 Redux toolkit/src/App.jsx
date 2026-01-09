812518f232e41b9048ba2037bb7f2c5df5473ba8
Aditya Thorat <a.workspace.00@gmail.com>
2026-01-09 11:04:36 +0530

    redux implemented

---
+++ 13 Redux toolkit/src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./store/reducers/counterSlice";
+
+const App = () => {
+  const { value } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="m-auto container p-10 mt-5 bg-red-100">
+      <h1 className="text-center text-3xl font-bold text-red-900">
+        Counter: {value}
+      </h1>
+      <div className="text-center mt-5">
+        <button
+          onClick={() => {
+            dispatch(increment());
+          }}
+          className="border rounded bg-sky-200 py-1 px-2 border border-black m-2"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="border rounded bg-sky-200 py-1 px-2 border border-black m-2"
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => dispatch(incrementByAmount(5))}
+          className="border rounded bg-sky-200 py-1 px-2 border border-black m-2"
+        >
+          Increment by 5
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
