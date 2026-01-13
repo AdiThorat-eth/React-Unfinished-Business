ef80a891b964c60fb0ea1d247adc8d4d61b6d118
Aditya Thorat <a.workspace.00@gmail.com>
2026-01-13 17:01:17 +0530

    refactor: remove unused buttons and integrate SearchBar component

--- 16 Redux mini project/src/App.jsx
+++ 16 Redux mini project/src/App.jsx
@@ -1,38 +1,10 @@
 import React from "react";
-import { fetchGif, fetchPhotos, fetchVideos } from "./api/MediaApi";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
     <div className="h-screen w-full bg-gray-950 text-white">
-      <button
-        onClick={async () => {
-          const data = await fetchPhotos("cat");
-          console.log(data.results);
-        }}
-        className="px-2 py-1 bg-sky-500 text-2xl rounded-lg m-10"
-      >
-        Get photos
-      </button>
-
-      <button
-        onClick={async () => {
-          const data = await fetchVideos("cat");
-          console.log(data.videos);
-        }}
-        className="px-2 py-1 bg-violet-500 text-2xl rounded-lg m-10"
-      >
-        Get Videos
-      </button>
-
-      <button
-        onClick={async () => {
-          const data = await fetchGif("cat");
-          console.log(data.data);
-        }}
-        className="px-2 py-1 bg-amber-500 text-2xl rounded-lg m-10"
-      >
-        Get Gifs
-      </button>
+      <SearchBar />
     </div>
   );
 };
